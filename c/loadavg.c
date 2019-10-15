b4cb87b3a880f47c8b456393ea29a512346ca5d6
Tim Hammerquist <thammerquist@chegg.com>
2019-10-15 14:11:10 -0700

    C version too complex
    I used the C program as a playground for new techniques/approaches, but
    it got stupid-complicated. Cleaned out all the cruft. Also a lot
    smaller.

--- c/loadavg.c
+++ c/loadavg.c
@@ -1,80 +1,20 @@
 /* -*- mode: c -*- */
-
 /*
  * loadavg.c
  */
 
-#include <inttypes.h>           /* strtoimax */
-#include <stdio.h>              /* fprintf, fputs, snprintf */
-#include <stdlib.h>             /* exit, getloadavg(macOS) */
-#include <unistd.h>             /* getopt */
+#include <stdio.h>              /* fprintf, */
+#include <stdlib.h>             /* getloadavg(macOS) */
 
 
-#define BUFSIZE 128
+#define BUFSIZE 16
 #define MAX_SAMPLES 3
-#define USAGE() { fprintf(stdout, USAGE_FORMAT, argv[0]); exit(1); }
-#define WARN(msg) { fprintf(stderr, "%s: %s\n", argv[0], msg); }
-#define ERROR(msg) { WARN(msg); exit(1); }
 
 
-const char * const USAGE_FORMAT = "usage: %s [-s N]\n"
-  "\t-s N\tCollect N number of samples\n";
-
-
-int format_nums(double *nums, int c, char * const buf, int s);
-
-
-int main(int argc, char **argv) {
-  char buffer[BUFSIZE] = {0};
-  double loads[MAX_SAMPLES] = {0.0};
-  int count = MAX_SAMPLES;
-  int ch;
-
-  while ((ch = getopt(argc, argv, "s:")) != -1) {
-    switch (ch) {
-    case 's':
-      count = (int)strtoimax(optarg, NULL, 10);
-      if (count < 1 || count > MAX_SAMPLES) {
-        USAGE();
-        ERROR("count out of range");
-      }
-      break;
-    case '?':
-    default:
-      USAGE();
-      break;
-    }
-  }
-  argc -= optind;
-  argv += optind;
-
-  if (getloadavg(loads, count) < count) {
-    ERROR("failed to collect load samples");
-  }
-
-  int written = format_nums(loads, count, buffer, sizeof(buffer));
-  if (written < 0) {
-    ERROR("no format written");
-  }
-
-  fputs(buffer, stdout);
-
+int main() {
+  double ls[MAX_SAMPLES] = {0.0};
+  getloadavg(ls, MAX_SAMPLES);
+  printf("%03.2f %03.2f %03.2f\n", ls[0], ls[1], ls[2]);
   return 0;
 }
 
-
-int
-format_nums(double *nums, int c, char * const buf, int s) {
-  char *cur = buf;
-
-  while (c-- > 0) {
-    int n = snprintf(cur, (size_t)(s - (cur - buf)), "%03.2f ", *nums++);
-    if (n < 0) {
-      return (-1);
-    }
-    cur += n;
-  }
-  *(cur - 1) = '\n';
-
-  return (int)(cur - buf);
-}
