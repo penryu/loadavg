abbc05c0eccecf9bab6ae89bc872fdc02786d234
Tim Hammerquist <tim@penryu.dev>
2023-04-18 04:48:32 -0700

    refactor and simplify

--- rs/src/main.rs
+++ rs/src/main.rs
@@ -1,45 +1,72 @@
-#![warn(clippy::all, clippy::pedantic)]
+#![warn(clippy::pedantic)]
 
-mod temp;
-
+use libc::getloadavg;
 use std::fmt;
 
-extern "C" {
-    fn getloadavg(loadavg: *mut f64, count: i32) -> i32;
-}
-
-pub struct LoadAvg {
-    one: f64,
-    five: f64,
-    fifteen: f64,
-}
-
-impl Default for LoadAvg {
-    fn default() -> Self {
-        LoadAvg {
-            one: 0.0,
-            five: 0.0,
-            fifteen: 0.0,
-        }
-    }
-}
+/// Holds load readings.
+#[derive(Debug, Default)]
+pub struct LoadAvg(f64, f64, f64);
 
 impl fmt::Display for LoadAvg {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let LoadAvg { one, five, fifteen } = self;
-        write!(f, "{one:0.2} {five:0.2} {fifteen:0.2}")
+        let LoadAvg(x, y, z) = self;
+        write!(f, "{x:0.2} {y:0.2} {z:0.2}")
     }
 }
 
 fn main() {
     let mut loadavg = LoadAvg::default();
 
-    unsafe {
-        let _ = getloadavg(std::ptr::addr_of_mut!(loadavg.one), 3);
-    }
+    let _ = unsafe { getloadavg(std::ptr::addr_of_mut!(loadavg.0), 3) };
 
-    match crate::temp::read() {
-        Some(temp) => println!("{loadavg} / {temp:.1}°C"),
-        None => println!("{loadavg}"),
-    };
+    if let Ok(temp) = crate::temperature::read() {
+        println!("{loadavg} / {temp:.1}°C");
+    } else {
+        println!("{loadavg}");
+    }
+}
+
+#[cfg(target_os = "linux")]
+mod temperature {
+    use anyhow::Result;
+    use glob::glob;
+    use std::fs::read_to_string;
+
+    const SYS_PATH_TEMP: &str = "/sys/class/thermal/thermal_zone*/temp";
+
+    pub fn read() -> Result<f64> {
+        let readings = glob(SYS_PATH_TEMP)?
+            .flatten()
+            .flat_map(read_to_string)
+            .flat_map(|s| s.trim().parse::<f64>())
+            .collect::<Vec<_>>();
+
+        #[allow(clippy::cast_precision_loss)]
+        let count: f64 = readings.len() as f64;
+        let sum: f64 = readings.into_iter().sum();
+        let avg = sum / count;
+        Ok(avg / 1000.0)
+    }
+}
+
+#[cfg(target_os = "macos")]
+mod temperature {
+    use anyhow::{bail, Context, Result};
+    use std::process::Command;
+
+    const MACOS_TEMP_BIN: &str = "osx-cpu-temp";
+
+    pub fn read() -> Result<f64> {
+        let output = Command::new(MACOS_TEMP_BIN).output()?.stdout;
+        let line = String::from_utf8(output)?;
+        let temp = line
+            .split('°')
+            .next()
+            .context("Unrecognized input from {MACOS_TEMP_BIN}")?;
+
+        match temp.parse::<f64>() {
+            Ok(value) if value > 1.0 => Ok(value),
+            Ok(_) | Err(_) => bail!("temperature unavailable"),
+        }
+    }
 }
