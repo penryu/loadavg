5e84f2c7b337ddf9af83962bf9c918882bc42bb9
Tim Hammerquist <tim@penryu.dev>
2023-08-30 16:15:18 -0700

    chore: refactor/simplify loadavg.c

--- c/loadavg.c
+++ c/loadavg.c
@@ -1,20 +1,18 @@
 /* -*- mode: c -*- */
+
 /*
  * loadavg.c
  */
 
-#include <stdio.h>              /* fprintf, */
-#include <stdlib.h>             /* getloadavg(macOS) */
+#include <stdio.h>              /* fprintf */
+#include <stdlib.h>             /* getloadavg */
 
-
-#define BUFSIZE 16
 #define MAX_SAMPLES 3
 
+static double ls[MAX_SAMPLES] = {0.0};
 
-int main() {
-  double ls[MAX_SAMPLES] = {0.0};
+int main(void) {
   getloadavg(ls, MAX_SAMPLES);
   printf("%03.2f %03.2f %03.2f\n", ls[0], ls[1], ls[2]);
   return 0;
 }
-
