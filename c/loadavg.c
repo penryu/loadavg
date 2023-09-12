93dcb40efde82d17399715d1ded21f4784e73ada
Tim Hammerquist <tim@penryu.dev>
2023-09-11 23:14:00 -0700

    restore solaris to C; ignore vscode configs

--- c/loadavg.c
+++ c/loadavg.c
@@ -1,18 +1,19 @@
 /* -*- mode: c -*- */
 
-/*
- * loadavg.c
- */
+/* loadavg.c */
 
 #include <stdio.h>              /* fprintf */
-#include <stdlib.h>             /* getloadavg */
+#include <stdlib.h>             /* getloadavg(FreeBSD, Linux, macOS) */
 
-#define MAX_SAMPLES 3
+#if defined(__sun) && defined(__SVR4)
+#include <sys/time.h>           /* hrtime_t (used in `sys/loadavg.h`) */
+#include <sys/loadavg.h>        /* getloadavg(smartOS) */
+#endif
 
-static double ls[MAX_SAMPLES] = {0.0};
+static double Samples[3];
 
 int main(void) {
-  getloadavg(ls, MAX_SAMPLES);
-  printf("%03.2f %03.2f %03.2f\n", ls[0], ls[1], ls[2]);
+  getloadavg(Samples, 3);
+  printf("%03.2f %03.2f %03.2f\n", Samples[0], Samples[1], Samples[2]);
   return 0;
 }
